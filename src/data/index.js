1303f7af7dff3a2004d5474087e2aa02b1f7c1ef
roxdavirox <roxdavirox@gmail.com>
2019-04-25 13:54:12 -0300

    mongodb config

---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { user, pass } = require('../config/mongodb');
+
+const url = `mongodb+srv://${user}:${pass}@mktp-cluster-icaeh.mongodb.net/test?retryWrites=true`;
+
+mongoose.connect(
+  url,
+  { useNewUrlParser: true }
+);
+
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
