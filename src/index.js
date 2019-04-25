5d5b6d9ccfdc82326195e3d048eb00858e43c19f
roxdavirox <roxdavirox@gmail.com>
2019-04-25 18:20:42 -0300

    cors adicionado no middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('ok');
