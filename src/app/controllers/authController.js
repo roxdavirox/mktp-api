0edf6ab97c760173e69dedafb9da710002adf4ba
roxdavirox <roxdavirox@gmail.com>
2019-07-08 16:35:18 -0300

    retornando userId no token

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -48,70 +48,7 @@ router.post('/authenticate', async (req, res) => {
   
   user.password = undefined;
 
-  return res.send({ user, token: createToken({ id: user.id }) });
+  return res.send({ user, token: createToken({ userId: user.id }) });
 });
-
-// router.post('/forgot_password', async (req, res) => {
-//   const { email } = req.body;
-//   try {
-
-//     const user = await User.findOne({ email });
-
-//     if (!user) 
-//       return res.status(400).send({ error: 'User not found'});
-
-//     const token = crypto.randomBytes(20).toString('hex');
-
-//     const expires = new Date();
-//     expires.setHours(expires.getHours() + 1);
-
-//     await User.findByIdAndUpdate(user.id, {
-//       '$set': {
-//         passwordResetToken: token,
-//         passwordResetExpires: expires
-//       }
-//     });
-
-//     mailer.sendMail({
-//       to: email,
-//       from: 'roxdavirox@gmail.com',
-//       template: 'auth/forgot_password',
-//       context: { token }
-//     }, (err) => {
-//       if (err) 
-//         return res.status(400).send({ error: 'Cannot send forgot password to email'});
-
-//       return res.send();
-//     });
-
-//   } catch(err) {
-//     res.status(400).send({ error: 'Error on forgot password, try again' + err });
-//   }
-// });
-
-// router.post('/reset_password', async (req, res) => {
-//   const { email, token, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email })
-//       .select('+passwordResetToken passwordResetExpires');
-
-//     if (token !== user.passwordResetToken) 
-//       return res.status(400).send({ error: 'Invalid token'});
-
-//     const now = new Date();
-
-//     if (now > user.passwordResetExpires)
-//       return res.status(400).send({ error: 'Expired token, generate a new one'});
-    
-//     user.password = password;
-
-//     await user.save();
-
-//     res.send();
-//   } catch (err) {
-//     res.status(400).send({ error: 'Cannot reset password, try again'});
-//   }
-// })
 
 module.exports = app => app.use('/auth', router);
