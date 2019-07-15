d534ef3908595145a4c655455ddcb7911fac4f21
roxdavirox <roxdavirox@gmail.com>
2019-07-15 16:22:03 -0300

    random file name with uuid

--- src/services/azureStorage.js
+++ src/services/azureStorage.js
@@ -9,8 +9,8 @@ const {
   uploadFileToBlockBlob
 } = require('@azure/storage-blob');
 
-const fs = require("fs");
-const path = require("path");
+const uuid = require('uuid/v1');
+
 const streamifier = require('streamifier');
 
 if (process.env.NODE_ENV !== "production") {
@@ -24,71 +24,15 @@ const ONE_MEGABYTE = 1024 * 1024;
 const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
 const ONE_MINUTE = 60 * 1000;
 
-async function showContainerNames(aborter, serviceURL) {
-
-  let response;
-  let marker;
-
-  do {
-      response = await serviceURL.listContainersSegment(aborter, marker);
-      marker = response.marker;
-      for(let container of response.containerItems) {
-          console.log(` - ${ container.name }`);
-      }
-  } while (marker);
-}
-
-async function uploadLocalFile(aborter, containerURL, filePath) {
-
-  filePath = path.resolve(filePath);
-
-  const fileName = path.basename(filePath);
-  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName);
-
-  return await uploadFileToBlockBlob(aborter, filePath, blockBlobURL);
-}
-
-async function uploadStream(aborter, containerURL, filePath) {
-
-  filePath = path.resolve(filePath);
-
-  const fileName = path.basename(filePath).replace('.md', '-stream.md');
-  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName);
-
-  const stream = fs.createReadStream(filePath, {
-    highWaterMark: FOUR_MEGABYTES,
-  });
-
-  const uploadOptions = {
-      bufferSize: FOUR_MEGABYTES,
-      maxBuffers: 5,
-  };
-
-  return await uploadStreamToBlockBlob(
-                  aborter, 
-                  stream, 
-                  blockBlobURL, 
-                  uploadOptions.bufferSize, 
-                  uploadOptions.maxBuffers);
-}
-
-async function showBlobNames(aborter, containerURL) {
-
-  let response;
-  let marker;
-
-  do {
-      response = await containerURL.listBlobFlatSegment(aborter);
-      marker = response.marker;
-      for(let blob of response.segment.blobItems) {
-          console.log(` - ${ blob.name }`);
-      }
-  } while (marker);
+function getRandomFileName(filename) {
+  const [_, extensionFile] = filename.split('.');
+  const newFileName = `${uuid()}.${extensionFile}`;
+  return newFileName;
 }
 
 async function uploadImage(imageFile) {
-  const { originalname: fileName, buffer } = imageFile;
-
+  const { originalfilename, buffer } = imageFile;
+  
   const containerName = "fotos-produtos";
   const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
   const pipeline = StorageURL.newPipeline(credentials);
@@ -97,6 +41,8 @@ async function uploadImage(imageFile) {
   const aborter = Aborter.timeout(30 * ONE_MINUTE);
   
   const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+  
+  const fileName = getRandomFileName(originalfilename);
   const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName);
 
   const stream = streamifier.createReadStream(buffer);
