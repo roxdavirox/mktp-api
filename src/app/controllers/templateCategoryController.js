e4388529b0b68228f83574c53f2dd048d27a1826
roxdavirox <roxdavirox@gmail.com>
2019-08-26 17:08:17 -0300

    getAllTemplatesCategory

--- src/app/controllers/templateCategoryController.js
+++ src/app/controllers/templateCategoryController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const TemplateCategory = require('../models/templateCategory');
 const Product = require('../models/product');
 
-const getAllTemplatesCategory = async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const templatesCategory = await TemplateCategory.find();
     return res.send({ templatesCategory });
@@ -13,9 +13,7 @@ const getAllTemplatesCategory = async (req, res) => {
     return res.status(400)
     .send({ error: `Error when get all templates category ${e}`});
   }
-};
-
-router.get('/', getAllTemplatesCategory);
+});
 
 router.get('/:productId', async (req, res) => {
   try {
