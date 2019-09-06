ec3fbf752638c45ae1b3105d055c15453e489dbd
roxdavirox <roxdavirox@gmail.com>
2019-09-06 19:11:11 -0300

    enviando buffer com formData para api cc

--- src/services/customerCanvas.js
+++ src/services/customerCanvas.js
@@ -12,22 +12,23 @@ function getRandomFileName(originalfilename) {
 async function uploadPsdToCustomerCanvas(psdFile) {
     const { originalname } = psdFile;
     const newFileName = getRandomFileName(originalname);
-    const newPsdFile = { ...psdFile, originalname: newFileName };
-    // console.log('psd File', newPsdFile);
-    // fazer request para CC aqui
+  
     const customerCanvasUrlApi = process.env.CUSTOMER_CANVAS_URL_API;
     const ccSecurityKey = process.env.CUSTOMER_CANVAS_SECURITY_KEY;
     const url = `${customerCanvasUrlApi}/ProductTemplates/designs/test`;
-    const data = new FormData();
+    const fd = new FormData();
     console.log('enviando psd para url:', url);
-    data.append('test', newPsdFile);
-    const config = {
-        headers: 
-        { 
+    const { buffer } = psdFile;
+    const newBuffer = new Buffer.from(buffer);
+    fd.append('file', newBuffer, { filename: newFileName, contentType: 'application/octet-stream' });
+    const options = {
+        headers: { 
             'X-CustomersCanvasAPIKey': ccSecurityKey, 
-            'Content-Type': 'multipart/form-data' 
-    }};
-    axios.post(url, data, config)
+            'Content-Type': 'multipart/form-data',
+            'Content-Type': fd.getHeaders()['content-type']
+        }
+    };
+    axios.post(url, fd, options)
         .then(res => console.log('response axios cc:', res))
         .catch(err => console.log('erro no request do cc:', err));
 }
