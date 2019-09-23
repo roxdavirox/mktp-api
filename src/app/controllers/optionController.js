f0ac5fd8b9535b3b3b43dc452847ea60e5168da4
roxdavirox <roxdavirox@gmail.com>
2019-09-23 17:55:39 -0300

    option controller fix style guide airbnb

--- src/app/controllers/optionController.js
+++ src/app/controllers/optionController.js
@@ -5,32 +5,32 @@ const Item = require('../models/item');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+const createOption = async (req, res) => {
   try {
     const option = await Option.create(req.body);
 
     return res.send({ option });
   } catch (e) {
-    return res.status(400).send({ error: 'Error when try to create a option'});
+    return res.status(400).send({ error: 'Error when try to create a option' });
   }
-});
+};
 
-router.get('/', async (req, res) => {
+const getAllOptions = async (req, res) => {
   try {
     const options = await Option.find().populate('items');
 
     return res.send({ options });
   } catch (e) {
-    return res.status(400).send({ error: 'Error on load options'});    
+    return res.status(400).send({ error: 'Error on load options' });
   }
-});
+};
 
-router.put('/', async (req, res) => {
+const updateItemIntoOptions = async (req, res) => {
   try {
     const { optionId, itemId } = req.body;
-    
+
     const option = await Option.findById(optionId).populate('items');
-    
+
     const item = await Item.findById(itemId);
 
     option.items.pull(itemId);
@@ -41,41 +41,45 @@ router.put('/', async (req, res) => {
 
     return res.send({ option });
   } catch (err) {
-    return res.status(400).send({ error: 'Error on update option'});
+    return res.status(400).send({ error: 'Error on update option' });
   }
-});
+};
 
-router.put('/:optionId', async (req, res) => {
+const addExistingItemsIntoOptions = async (req, res) => {
   try {
     const { optionId } = req.params;
-    console.log('put optionId:', optionId);
     const { itemsId } = req.body;
-    console.log('ids dos itens:', itemsId);
-    
-    const option = await Option.findOneAndUpdate({ _id: optionId }, { 
-        $addToSet: { items: { $each: itemsId } }
-      }
-    );
+
+    const option = await Option
+      .findOneAndUpdate({ _id: optionId }, {
+        $addToSet: { items: { $each: itemsId } },
+      });
 
     await option.save();
 
     return res.send({ itemsCount: itemsId.length });
   } catch (err) {
     return res.status(400)
-      .send({ error: 'Error on add existing items into option'});
+      .send({ error: 'Error on add existing items into option' });
   }
-});
+};
 
-router.delete('/', async (req, res) => {
+const deleteManyOptionsByIds = async (req, res) => {
   try {
     const { optionsId } = req.body;
 
-    await Option.deleteMany({ _id:{ $in: optionsId }});
+    await Option.deleteMany({ _id: { $in: optionsId } });
 
     return res.send({ deletedOptionsCount: optionsId.length });
-  } catch(e) {
-    return res.status(400).send({ error: `Error on deleting option(s): ${e}`});
+  } catch (e) {
+    return res.status(400).send({ error: `Error on deleting option(s): ${e}` });
   }
-});
+};
 
-module.exports = app => app.use('/options', router);
+router.post('/', createOption);
+router.get('/', getAllOptions);
+router.put('/', updateItemIntoOptions);
+router.put('/:optionId', addExistingItemsIntoOptions);
+router.delete('/', deleteManyOptionsByIds);
+
+module.exports = (app) => app.use('/options', router);
