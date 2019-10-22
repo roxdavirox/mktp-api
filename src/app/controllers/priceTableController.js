8c88eb919c8d3727e4b54d2e6ee32632af5384e1
Felipe Freitas <felfreitas88@gmail.com>
2019-10-22 20:06:52 -0300

    calcular pela quantidade

--- src/app/controllers/priceTableController.js
+++ src/app/controllers/priceTableController.js
@@ -19,7 +19,38 @@ const createNewPriceTable = async (req, res) => {
 };
 
 // TODO: Calcular total quando a unidade for quantidade
-const getPriceValue = async (req, res) => {
+const calculatePriceByQuantity = async (req, res) => {
+  try {
+    const { priceTableId } = req.params;
+    const { quantity } = req.body;
+    const totalArea = quantity;
+
+    const priceTable = await PriceTable.findById(priceTableId)
+      .populate('prices');
+
+    const { prices } = priceTable;
+    const preco = prices.find((price) => (price.start <= totalArea && totalArea <= price.end));
+
+    if (!preco) {
+      const lastPrice = prices[prices.length - 1];
+      const total = lastPrice.value * totalArea;
+      return res.send({
+        total,
+      });
+    }
+
+    const total = preco.value * totalArea;
+
+    return res.send({
+      total,
+    });
+  } catch (e) {
+    return res.status(400)
+      .send({ error: `Error on get price tables: ${e}` });
+  }
+};
+
+const calculatePriceByArea = async (req, res) => {
   try {
     const { priceTableId } = req.params;
     const { quantity, size: { x, y } } = req.body;
@@ -112,7 +143,8 @@ const createPriceTableChild = async (req, res) => {
 };
 
 router.post('/', createNewPriceTable);
-router.post('/total/:priceTableId', getPriceValue);
+router.post('/total-area/:priceTableId', calculatePriceByArea);
+router.post('/total-quantity/:priceTableId', calculatePriceByArea);
 router.get('/', getPriceTables);
 router.get('/:priceTableId', getPricesByPriceTableId);
 router.delete('/', deleteManyPriceTablesByIds);
