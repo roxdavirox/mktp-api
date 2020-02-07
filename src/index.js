047362fbeb754bef34624e17359224865d74e600
roxdavirox <roxdavirox@gmail.com>
2020-02-07 17:12:50 -0300

    logs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,8 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-console.log(`${__dirname}`);
-app.set('views', `${__dirname}/views`);
+console.log(`[mktp] servindo no diretório: ${__dirname}`);
+console.log('[mktp] configurando react-views');
+app.set('views', './app/views');
 app.set('view engine', 'jsx');
 const engine = require('express-react-views').createEngine();
 
@@ -18,8 +20,10 @@ app.engine('jsx', engine);
 
 require('dotenv').config();
 
+console.log('[mktp] iniciando rotas');
 require('./app/controllers/index')(app);
 
 const port = process.env.PORT || 3001;
 
+console.log('[mktp] online na porta: ', port);
 app.listen(port);
