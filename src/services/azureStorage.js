5dad66f224219dcf7fa1a3b73a792b9571db6740
roxdavirox <roxdavirox@gmail.com>
2020-02-07 17:16:56 -0300

    semicolon

--- src/services/azureStorage.js
+++ src/services/azureStorage.js
@@ -9,82 +9,82 @@ const {
   SharedKeyCredential,
   StorageURL,
   uploadStreamToBlockBlob,
-} = require('@azure/storage-blob')
+} = require('@azure/storage-blob');
 
-const uuid = require('uuid/v1')
+const uuid = require('uuid/v1');
 
-const streamifier = require('streamifier')
+const streamifier = require('streamifier');
 
-const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME
-const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY
-console.log('iniciando variaveis de ambiente')
+const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+console.log('iniciando variaveis de ambiente');
 
-const ONE_MEGABYTE = 1024 * 1024
-const FOUR_MEGABYTES = 4 * ONE_MEGABYTE
-const ONE_MINUTE = 60 * 1000
+const ONE_MEGABYTE = 1024 * 1024;
+const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
+const ONE_MINUTE = 60 * 1000;
 
 function getRandomFileName(originalfilename) {
-  const [, ext] = originalfilename.split('.')
-  console.log('extension:', ext)
-  const newFileName = `${uuid()}.${ext}`
-  return newFileName
+  const [, ext] = originalfilename.split('.');
+  console.log('extension:', ext);
+  const newFileName = `${uuid()}.${ext}`;
+  return newFileName;
 }
 
 async function productImageUpload(imageFile) {
-  return await uploadImage(imageFile, 'fotos-produtos')
+  return await uploadImage(imageFile, 'fotos-produtos');
 }
 
 async function templatePreviewUpload(imageFile) {
-  return await uploadImage(imageFile, 'preview-templates')
+  return await uploadImage(imageFile, 'preview-templates');
 }
 
 async function uploadImage(imageFile, containerName) {
-  const { originalname, buffer } = imageFile
+  const { originalname, buffer } = imageFile;
 
   // const containerName = "fotos-produtos";
-  console.log('nome do container:', containerName)
-  console.log('autenticando azure credentials')
-  console.log('NAME:', STORAGE_ACCOUNT_NAME, ' KEY:', ACCOUNT_ACCESS_KEY)
-  const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY)
-  console.log('autenticado com sucesso...')
-  const pipeline = StorageURL.newPipeline(credentials)
-  console.log('criando pipeline do azure blobs')
-  const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline)
-  console.log('iniciando serviços azure blob')
-  const aborter = Aborter.timeout(30 * ONE_MINUTE)
+  console.log('nome do container:', containerName);
+  console.log('autenticando azure credentials');
+  console.log('NAME:', STORAGE_ACCOUNT_NAME, ' KEY:', ACCOUNT_ACCESS_KEY);
+  const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
+  console.log('autenticado com sucesso...');
+  const pipeline = StorageURL.newPipeline(credentials);
+  console.log('criando pipeline do azure blobs');
+  const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
+  console.log('iniciando serviços azure blob');
+  const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
-  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName)
+  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
 
-  const fileName = getRandomFileName(originalname)
-  console.log('fileName:', fileName)
-  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName)
+  const fileName = getRandomFileName(originalname);
+  console.log('fileName:', fileName);
+  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName);
 
-  const stream = streamifier.createReadStream(buffer)
-  console.log('criando stream')
+  const stream = streamifier.createReadStream(buffer);
+  console.log('criando stream');
   const uploadOptions = {
     bufferSize: FOUR_MEGABYTES,
     maxBuffers: 5,
-  }
-  console.log('enviando arquivo')
+  };
+  console.log('enviando arquivo');
   const response = await uploadStreamToBlockBlob(
     aborter,
     stream,
     blockBlobURL,
     uploadOptions.bufferSize,
     uploadOptions.maxBuffers,
-  )
-  console.log('arquivo enviado')
-  const { url } = serviceURL
-  const imageUrl = `${url}${containerName}/${fileName}`
-  console.log('url da imagem:', imageUrl)
+  );
+  console.log('arquivo enviado');
+  const { url } = serviceURL;
+  const imageUrl = `${url}${containerName}/${fileName}`;
+  console.log('url da imagem:', imageUrl);
   return {
     imageUrl,
     ...response,
-  }
+  };
 }
 
 module.exports = {
   productImageUpload,
   templatePreviewUpload,
   uploadImage,
-}
+};
--- src/services/customerCanvas.js
+++ src/services/customerCanvas.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-console */
-const uuid = require('uuid/v1')
-const axios = require('axios')
-const FormData = require('form-data')
+const uuid = require('uuid/v1');
+const axios = require('axios');
+const FormData = require('form-data');
 
 function getRandomFileName(originalfilename) {
   // eslint-disable-next-line no-unused-vars
-  const [_, ext] = originalfilename.split('.')
-  console.log('extension:', ext)
-  const newFileName = `${uuid()}.${ext}`
-  return newFileName
+  const [_, ext] = originalfilename.split('.');
+  console.log('extension:', ext);
+  const newFileName = `${uuid()}.${ext}`;
+  return newFileName;
 }
 
 async function uploadPsdToCustomerCanvas(psdFile) {
-  const { originalname } = psdFile
-  const newFileName = getRandomFileName(originalname)
+  const { originalname } = psdFile;
+  const newFileName = getRandomFileName(originalname);
 
-  const customerCanvasUrlApi = process.env.CUSTOMER_CANVAS_URL_API
-  const ccSecurityKey = process.env.CUSTOMER_CANVAS_SECURITY_KEY
-  const url = `${customerCanvasUrlApi}/ProductTemplates/designs/test`
-  const fd = new FormData()
-  console.log('enviando psd para url:', url)
-  const { buffer } = psdFile
+  const customerCanvasUrlApi = process.env.CUSTOMER_CANVAS_URL_API;
+  const ccSecurityKey = process.env.CUSTOMER_CANVAS_SECURITY_KEY;
+  const url = `${customerCanvasUrlApi}/ProductTemplates/designs/test`;
+  const fd = new FormData();
+  console.log('enviando psd para url:', url);
+  const { buffer } = psdFile;
   // eslint-disable-next-line new-cap
-  const newBuffer = new Buffer.from(buffer)
-  fd.append('file', newBuffer, { filename: newFileName, contentType: 'application/octet-stream' })
+  const newBuffer = new Buffer.from(buffer);
+  fd.append('file', newBuffer, { filename: newFileName, contentType: 'application/octet-stream' });
   const options = {
     headers: {
       'X-CustomersCanvasAPIKey': ccSecurityKey,
@@ -31,12 +31,12 @@ async function uploadPsdToCustomerCanvas(psdFile) {
       // eslint-disable-next-line no-dupe-keys
       'Content-Type': fd.getHeaders()['content-type'],
     },
-  }
+  };
 
-  const response = await axios.post(url, fd, options)
-  return response
+  const response = await axios.post(url, fd, options);
+  return response;
 }
 
 module.exports = {
   uploadPsdToCustomerCanvas,
-}
+};
