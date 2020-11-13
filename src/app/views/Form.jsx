d7181d482868e9dbb84f47bf76ec3115a1dd13d1
roxdavirox <roxdavirox@gmail.com>
2020-11-13 16:38:40 -0300

    new css flex box

--- src/app/views/Form.jsx
+++ src/app/views/Form.jsx
@@ -1,21 +1,9 @@
 var React = require("react");
 
 const styles = {
-  container: {
-    display: 'table-caption',
-    maxWidth: '450px',
-    justifyContent: 'space-between'
-  },
   label: {
     width: '150px',
     margin: 'auto'
-  },
-  select: {
-    width: '450px'
-  },
-  input: {
-    width: '450px',
-    maxWidth: '4500px'
   },
   span: {
     width: '200px'
@@ -23,6 +11,28 @@ const styles = {
   erro: {
     color: 'red',
     display: 'none'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    width: '-webkit-fill-available'
+  },
+  box: {
+    flexGrow: 1,
+    padding: '1px',
+    width: '200px',
+  },
+  boxItem: {
+    width: '100%',
+    border: 'solid 1px black',
+    padding: '11.25px',
+    minHeight: '46.5px'
+  },
+  boxInput: {
+    flexGrow: 1,
+    display: 'flex',
+    alignContent: 'center',
+    alignItems: 'center',
   }
 }
 
@@ -30,134 +40,183 @@ const Form = ({ sizeSelectedIndex = 0, options, sortedOptionsId, sizes, selected
 
   return (
     <>
-      <form>
-        <div >
-          <label htmlFor="quantity-select" >Quantidade:</label>
-          <input 
-            type="number"
-            defaultValue="1"
-            id="quantity-select"
-             />
-        </div>
-        <div >
-          {sizes && <label htmlFor="size-select">Medidas</label>}
-          {sizes && 
-            <select id="size-select" >
-              {sizes.map((size, index) => 
-                <option 
-                  key={index} 
-                  _size={JSON.stringify(size)} 
-                  selected={index == sizeSelectedIndex}>
-                    {size.x}x{size.y}{unit}
-                </option>)}
-            </select>
-          }
-        </div>
-        {sortedOptionsId.map(k =>
-          <div _optionId={options[k]._id} id="item-container">
-            <div key={k} className="elementor-row">
-              <div className="elementor-column elementor-col-100 elementor-inner-column elementor-element" style={{ display: 'block' }}>
-                <label for={options[k]._id} >{options[k].name}:</label>
-                <select
-                  className="item-select"
-                  id={options[k]._id}
-                  key={k}>
-                  {options[k].items.map(item => 
+      <div className="elementor-widget-container">
+        <div className="elementor-form-fields-wrapper">
+          <div className="elementor-column elementor-col-100 elementor-field-group">
+            <div
+              style={{ marginBottom:  '0px', border: '1px solid black' }}
+              className="elementor-column elementor-col-100 elementor-field-group"
+            >
+              Quantidade:
+            </div>
+            <div className="elementor-column elementor-col-100 elementor-field-group">
+              <input 
+                type="number"
+                defaultValue="1"
+                id="quantity-select"
+                className="elementor-field elementor-size-sm  elementor-field-textual"
+              />
+            </div>
+          </div>
+          <div className="elementor-column elementor-col-100 elementor-field-group">
+            {sizes &&
+              <div
+                style={{ marginBottom:  '0px;' }}
+                className="elementor-column elementor-col-100 elementor-field-group"
+              >
+                Medidas:
+              </div>
+            }
+            <div className="elementor-column elementor-col-100 elementor-field-group">
+              {sizes && 
+                <select id="size-select" className="elementor-field elementor-size-sm  elementor-field-textual">
+                  {sizes.map((size, index) => 
                     <option 
-                      key={options[k]._id}
-                      id={options[k]._id}
-                      _itemid={item._id.toString()}
-                      _optionId={options[k]._id}
-                      _showUnitfield={`${item.showUnitField || false}`}
-                      _unit={item.priceTable ? item.priceTable : ''}
-                      selected={selectedItemsId.indexOf(item._id.toString()) !== -1}>
-                        {item.name}
+                      key={index} 
+                      _size={JSON.stringify(size)} 
+                      selected={index == sizeSelectedIndex}>
+                        {size.x}x{size.y}{unit}
                     </option>)}
                 </select>
-              </div>
-              {options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).showUnitField
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable.unit === 'quantidade'
-                && 
-                  <div _optionId={options[k]._id} className="elementor-column elementor-col-50 elementor-inner-column elementor-element" style={{ display: 'block' }}>
-                    <label for={`input-unit-quantity`} >{defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].label || 'Quantidade'}:</label>
-                    <input
-                      type="text"
-                      id={`input-unit-quantity`}
-                      _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
-                      placeholder="Quantidade"
-                      type="number"
-                      value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()] 
-                        ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].quantity
-                        : '1'}
-                    >
-                    </input>
-                  </div>
-              }
-              {options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).showUnitField
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable
-                && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable.unit !== 'quantidade'
-                && 
-                  <div _optionId={options[k]._id} className="elementor-row" style={{ maxHeight: '100%', display: 'block' }}>
-                    <label >{defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].label || 'Medida'}:</label>
-                    <div style={{ display: 'inline-flex' }}>
-                      <div className="elementor-column elementor-col-50 elementor-inner-column elementor-element">
-                        <input 
-                          type="text"
-                          id="input-unit-x"
-                          _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
-                          placeholder="Comp."
-                          type="number"
-                          value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()]
-                            ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].x
-                            : '1'}></input>
-                      </div>
-                      <div  className="elementor-column elementor-col-50 elementor-inner-column elementor-element">
-                        <input
-                          type="text"
-                          id="input-unit-y"
-                          _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
-                          placeholder="Larg."
-                          type="number"
-                          value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()]
-                            ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].y
-                            : '1'}
-                        >
-                        </input>
-                      </div>
-                    </div>
-                  </div>
               }
             </div>
           </div>
-        )}
-        <div  className="orcamento-inputs">
-          <label htmlFor="name" >Nome:</label>
-          <input type="text" id="name" ></input>
+
+          {sortedOptionsId.map(k =>
+            <div 
+              key={k}
+              _optionId={options[k]._id}
+              id="item-container"
+              // className="elementor-field-group"
+              style={styles.container}
+            >
+                <div style={styles.box}>
+                  <div
+                    style={{ marginBottom:  '0px;' }}
+                    // class="elementor-field-group"
+                  >{options[k].name}:</div>
+                  <div>
+                    <select
+                      className="item-select"
+                      style={styles.boxItem}
+                      id={options[k]._id}
+                      key={k}>
+                      {options[k].items.map(item => 
+                        <option
+                          key={options[k]._id}
+                          id={options[k]._id}
+                          _itemid={item._id.toString()}
+                          _optionId={options[k]._id}
+                          _showUnitfield={`${item.showUnitField || false}`}
+                          _unit={item.priceTable ? item.priceTable : ''}
+                          selected={selectedItemsId.indexOf(item._id.toString()) !== -1}>
+                            {item.name}
+                        </option>)}
+                    </select>
+                  </div>
+                </div>
+
+                {/* quantidade */}
+                {options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).showUnitField
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable.unit === 'quantidade'
+                  && 
+                    <div _optionId={options[k]._id} style={styles.box}>
+                      <div
+                        style={{ marginBottom:  '0px;' }}
+                        // className="elementor-column elementor-col-100 elementor-field-group"
+                      >
+                        {defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].label || 'Quantidade'}:
+                      </div>
+                      <div style={{ paddingLeft: '3px'}}>
+                        <input
+                          style={styles.boxItem}
+                          type="text"
+                          id={`input-unit-quantity`}
+                          _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
+                          placeholder="Quantidade"
+                          type="number"
+                          value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()] 
+                            ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].quantity
+                            : '1'}
+                          // className="elementor-field-textual"
+                        />
+                      </div>
+                    </div>
+                }
+
+                {/* medidas */}
+                {options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).showUnitField
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable
+                  && options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1).priceTable.unit !== 'quantidade'
+                  && 
+                    <div _optionId={options[k]._id} style={styles.box}>
+                      <div
+                        style={{ marginBottom:  '0px;' }}
+                        // className="elementor-column elementor-col-100 elementor-field-group"
+                      >
+                        {defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].label || 'Medida'}:
+                      </div>
+                      <div style={styles.boxInput}>
+                        <div style={{ paddingLeft: '3px', paddingRight: '5px' }}>
+                          <input 
+                            style={styles.boxItem}
+                            type="text"
+                            id="input-unit-x"
+                            _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
+                            placeholder="Comp."
+                            type="number"
+                            value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()]
+                              ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].x
+                              : '1'}></input>
+                        </div>
+                        <div>
+                          <input
+                            style={styles.boxItem}
+                            type="text"
+                            id="input-unit-y"
+                            _itemId={options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()}
+                            placeholder="Larg."
+                            type="number"
+                            value={defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()]
+                              ? defaultItems[options[k].items.find(item => selectedItemsId.indexOf(item._id.toString()) !== -1)._id.toString()].y
+                              : '1'}
+                          >
+                          </input>
+                        </div>
+                      </div>
+                    </div>
+                }
+            </div>
+          )}
+          <div  className="orcamento-inputs">
+            <label htmlFor="name" >Nome:</label>
+            <input type="text" id="name" ></input>
+          </div>
+          <div className="orcamento-inputs">
+            <p id="name-error" style={styles.erro}>Preencha o seu nome</p>
+          </div>
+          <div className="orcamento-inputs">
+            <label htmlFor="phone" placeholder="(__) _____-____">Telefone:</label>
+            <input type="text" id="phone" maxLength="15"></input>
+          </div>
+          <div className="orcamento-inputs">
+            <p id="phone-error" style={styles.erro}>Preencha seu telefone</p>
+          </div>
+          <div  className="orcamento-inputs">
+            <label htmlFor="email" >E-mail:</label>
+            <input type="text" id="email" ></input>
+          </div>
+          <div className="orcamento-inputs">
+            <p id="email-error" style={styles.erro}>E-mail inválido</p>
+          </div>
+          <div style={{ textAlign: 'right' }}>
+            <button id="ver-preco-button" className="single_add_to_cart_button button alt">Ver preço</button>
+          </div>
         </div>
-        <div className="orcamento-inputs">
-          <p id="name-error" style={styles.erro}>Preencha o seu nome</p>
-        </div>
-        <div className="orcamento-inputs">
-          <label htmlFor="phone" placeholder="(__) _____-____">Telefone:</label>
-          <input type="text" id="phone" maxLength="15"></input>
-        </div>
-        <div className="orcamento-inputs">
-          <p id="phone-error" style={styles.erro}>Preencha seu telefone</p>
-        </div>
-        <div  className="orcamento-inputs">
-          <label htmlFor="email" >E-mail:</label>
-          <input type="text" id="email" ></input>
-        </div>
-        <div className="orcamento-inputs">
-          <p id="email-error" style={styles.erro}>E-mail inválido</p>
-        </div>
-        <div style={{ textAlign: 'right' }}>
-          <button id="ver-preco-button" className="single_add_to_cart_button button alt">Ver preço</button>
-        </div>
-      </form>
+      </div>
     </>
   )
 };
